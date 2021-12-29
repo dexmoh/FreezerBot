0f4165ac48f2373ced4a0f2e78a8c027ab912125
Lazar Petrović <lazar.petrovic2310@gmail.com>
2021-12-29 19:00:54 +0100

    Remove namespace indentations

--- FreezerBot/Program.cs
+++ FreezerBot/Program.cs
@@ -5,85 +5,84 @@ using System.Threading.Tasks;
 using Discord;
 using Discord.WebSocket;
 
-namespace FreezerBot
+namespace FreezerBot;
+
+class Program
 {
-    class Program
+    // Discord client.
+    private readonly DiscordSocketClient _client;
+
+    // Flag used to check if the program is still running.
+    public static bool isRunning;
+
+    // Our bot's prefix.
+    private readonly string prefix;
+
+    // ID value associated with master account.
+    private readonly ulong masterId;
+
+    Program()
     {
-        // Discord client.
-        private readonly DiscordSocketClient _client;
+        isRunning = true;
 
-        // Flag used to check if the program is still running.
-        public static bool isRunning;
+        prefix = System.IO.File.ReadAllText(@"data\prefix.txt");
+        masterId = Convert.ToUInt64(System.IO.File.ReadAllText(@"data\master.txt"));
 
-        // Our bot's prefix.
-        private readonly string prefix;
+        _client = new DiscordSocketClient();
+        _client.Log += LogAsync;
+        _client.Ready += ReadyAsync;
+        _client.MessageReceived += MessageReceivedAsync;
 
-        // ID value associated with master account.
-        private readonly ulong masterId;
+        _client.SetGameAsync("AAAAAAAA", null, ActivityType.Listening);
+    }
 
-        Program()
-        {
-            isRunning = true;
+    public static Task Main(string[] args) => new Program().MainAsync();
 
-            prefix = System.IO.File.ReadAllText(@"data\prefix.txt");
-            masterId = Convert.ToUInt64(System.IO.File.ReadAllText(@"data\master.txt"));
+    public async Task MainAsync()
+    {
+        await _client.LoginAsync(TokenType.Bot, System.IO.File.ReadAllText(@"data\token.txt"));
+        await _client.StartAsync();
 
-            _client = new DiscordSocketClient();
-            _client.Log += LogAsync;
-            _client.Ready += ReadyAsync;
-            _client.MessageReceived += MessageReceivedAsync;
+        // Keep running the program until its told to shutdown.
+        while (isRunning)
+            await Task.Delay(3000);
 
-            _client.SetGameAsync("AAAAAAAA", null, ActivityType.Listening);
-        }
+        await _client.StopAsync();
+        Thread.Sleep(3000);
+    }
 
-        public static Task Main(string[] args) => new Program().MainAsync();
+    private Task LogAsync(LogMessage msg)
+    {
+        // Log the messages to a file.
+        StreamWriter sw = new StreamWriter(@"data\debug.log", true);
+        sw.WriteLine(msg.ToString());
+        sw.Close();
 
-        public async Task MainAsync()
-        {
-            await _client.LoginAsync(TokenType.Bot, System.IO.File.ReadAllText(@"data\token.txt"));
-            await _client.StartAsync();
+        Console.WriteLine(msg.ToString());
+        return Task.CompletedTask;
+    }
 
-            // Keep running the program until its told to shutdown.
-            while (isRunning)
-                await Task.Delay(3000);
+    private Task ReadyAsync()
+    {
+        Console.WriteLine($"{_client.CurrentUser} is connected!");
+        return Task.CompletedTask;
+    }
 
-            await _client.StopAsync();
-            Thread.Sleep(3000);
-        }
+    private async Task MessageReceivedAsync(SocketMessage msg)
+    {
+        string[] args = msg.Content.ToLower().Split(' ');
+        int argsLen = args.Length;
 
-        private Task LogAsync(LogMessage msg)
-        {
-            // Log the messages to a file.
-            StreamWriter sw = new StreamWriter(@"data\debug.log", true);
-            sw.WriteLine(msg.ToString());
-            sw.Close();
+        if (args[0] != prefix)
+            return;
 
-            Console.WriteLine(msg.ToString());
-            return Task.CompletedTask;
-        }
+        if (msg.Author.IsBot)
+            return;
 
-        private Task ReadyAsync()
-        {
-            Console.WriteLine($"{_client.CurrentUser} is connected!");
-            return Task.CompletedTask;
-        }
+        bool isMaster = false;
+        if (msg.Author.Id == masterId)
+            isMaster = true;
 
-        private async Task MessageReceivedAsync(SocketMessage msg)
-        {
-            string[] args = msg.Content.ToLower().Split(' ');
-            int argsLen = args.Length;
-            
-            if (args[0] != prefix)
-                return;
-
-            if (msg.Author.IsBot)
-                return;
-
-            bool isMaster = false;
-            if (msg.Author.Id == masterId)
-                isMaster = true;
-
-            await CommandHandler.HandleCommandAsync(msg, args, argsLen, prefix, isMaster);
-        }
+        await CommandHandler.HandleCommandAsync(msg, args, argsLen, prefix, isMaster);
     }
 }
